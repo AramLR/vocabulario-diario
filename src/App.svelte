f26b999f056ea8cd40a24c5792e1520987b97e66
AramLR <aramluck2007@gmail.com>
2023-07-25 20:10:40 -0600

    style

--- src/App.svelte
+++ src/App.svelte
@@ -3,30 +3,113 @@
     getRandomWord,
     getMeaning
   } from './lib/api'
+  import Loading from './lib/components/Loading.svelte'
 </script>
 
-{#await getRandomWord()}
-  Cargando...
-{:then word}
-  {#await getMeaning(word)}
-    Cargando...
-  {:then meaning}
-    {#if meaning.Error}
-      <h1>
-        No se encontró una
-        traducción para
-        esta palabra.
-      </h1>
-    {:else}
-      <h1>{word}</h1>
+<svelte:head>
+  <link
+    rel="preconnect"
+    href="https://fonts.googleapis.com"
+  />
+  <link
+    rel="preconnect"
+    href="https://fonts.gstatic.com"
+    crossOrigin=""
+  />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
 
-      <p>
-        {meaning.term0
-          .PrincipalTranslations[
-          '0'
-        ].OriginalTerm
-          .sense}
-      </p>
-    {/if}
+<section class="container">
+  {#await getRandomWord()}
+    <Loading />
+  {:then word}
+    {#await getMeaning(word)}
+      <Loading />
+    {:then meaning}
+      {#if meaning.Error}
+        <section
+          class="error-section"
+        >
+          <p class="error">
+            No se encontró
+            una traducción
+            para esta
+            palabra.
+          </p>
+
+          <button
+            on:click={() =>
+              window.location.reload()}
+            >Recargar</button
+          >
+        </section>
+      {:else}
+        <section
+          class="result"
+        >
+          <h1>{word}</h1>
+
+          <p>
+            {meaning.term0
+              .PrincipalTranslations[
+              '0'
+            ].OriginalTerm
+              .sense}
+          </p>
+
+          <button
+            on:click={() =>
+              window.location.reload()}
+            >Recargar</button
+          >
+        </section>
+      {/if}
+    {/await}
   {/await}
-{/await}
+</section>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+
+  .error-section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .error {
+    color: #c0392b;
+  }
+
+  button {
+    background-color: transparent;
+    height: 100%;
+    width: 80%;
+    padding: 10px;
+    letter-spacing: 5px;
+    border: 1px solid #000;
+    border-radius: 5px;
+    font-weight: bold;
+  }
+
+  .result {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  h1 {
+    font-size: 2em;
+    font-weight: 1000;
+  }
+</style>
---
+++ src/lib/components/Loading.svelte
@@ -0,0 +1,125 @@
+<div class="loader">
+  <span
+    class="loading-text"
+  >
+    <span class="letter"
+      >L</span
+    >
+    <span class="letter"
+      >o</span
+    >
+    <span class="letter"
+      >a</span
+    >
+    <span class="letter"
+      >d</span
+    >
+    <span class="letter"
+      >i</span
+    >
+    <span class="letter"
+      >n</span
+    >
+    <span class="letter"
+      >g</span
+    >
+  </span>
+  <div
+    class="dot-container"
+  >
+    <div class="dot" />
+    <div class="dot" />
+    <div class="dot" />
+  </div>
+</div>
+
+<style>
+  .loader {
+    display: flex;
+    align-items: center;
+    font-family: Arial,
+      sans-serif;
+    font-size: 2em;
+    color: #333;
+    font-weight: 1000;
+  }
+
+  .dot-container {
+    display: flex;
+    align-items: center;
+  }
+
+  .dot {
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background-color: #333;
+    margin: 0 5px;
+    animation: jump 1.5s
+      infinite;
+  }
+
+  .dot:nth-child(1) {
+    animation-delay: 2s;
+  }
+
+  .dot:nth-child(2) {
+    animation-delay: 2.25s;
+  }
+
+  .dot:nth-child(3) {
+    animation-delay: 2.5s;
+  }
+
+  .letter {
+    display: inline-block;
+    animation: jump 1.5s
+      infinite;
+  }
+
+  .letter:nth-child(1) {
+    animation-delay: 0.25s;
+  }
+
+  .letter:nth-child(2) {
+    animation-delay: 0.5s;
+  }
+
+  .letter:nth-child(3) {
+    animation-delay: 0.75s;
+  }
+
+  .letter:nth-child(4) {
+    animation-delay: 1s;
+  }
+
+  .letter:nth-child(5) {
+    animation-delay: 1.25s;
+  }
+
+  .letter:nth-child(6) {
+    animation-delay: 1.5s;
+  }
+
+  .letter:nth-child(7) {
+    animation-delay: 1.75s;
+  }
+
+  @keyframes jump {
+    0% {
+      transform: translateY(
+        0
+      );
+    }
+    50% {
+      transform: translateY(
+        -15px
+      );
+    }
+    100% {
+      transform: translateY(
+        0
+      );
+    }
+  }
+</style>
