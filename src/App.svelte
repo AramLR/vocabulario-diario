f7e3803b575cdaefc77d9ef98ef294f5f0d04d5a
AramLR <aramluck2007@gmail.com>
2023-07-25 18:52:05 -0600

    show word and meaning

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,32 @@
 <script lang="ts">
-  import { getRandomWord } from './lib/api'
+  import {
+    getRandomWord,
+    getMeaning
+  } from './lib/api'
 </script>
+
+{#await getRandomWord()}
+  Cargando...
+{:then word}
+  {#await getMeaning(word)}
+    Cargando...
+  {:then meaning}
+    {#if meaning.Error}
+      <h1>
+        No se encontró una
+        traducción para
+        esta palabra.
+      </h1>
+    {:else}
+      <h1>{word}</h1>
+
+      <p>
+        {meaning.term0
+          .PrincipalTranslations[
+          '0'
+        ].OriginalTerm
+          .sense}
+      </p>
+    {/if}
+  {/await}
+{/await}
